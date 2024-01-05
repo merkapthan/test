2a3dfb4f69961f8f2253ba8bc67bccdcf6a7a8f2
Konstantin Tsarkov <constantin.tsarkov@yandex.ru>
2024-01-05 23:37:02 +0400

    добавил инит для баллет_имаджес

---
+++ bullet2/bullet_images/__init__.py
@@ -0,0 +1,5 @@
+#инициализация для папки с изображениями 
+from tkinter import PhotoImage
+
+acorn_life = PhotoImage(file='bullet_images/acorn_life.gif')
+acorn_energy = PhotoImage(file='bullet_images/acorn_energy.gif')
